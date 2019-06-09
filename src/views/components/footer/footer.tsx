0e67e784668a13b35493d7390ab6936b183b4040
Hopsken <shaowei5@icloud.com>
2019-06-09 08:17:19 +0800

    feat: add Github Link to Header

--- src/views/components/footer/footer.tsx
+++ src/views/components/footer/footer.tsx
@@ -7,8 +7,11 @@ const kitty = require('@/views/assets/logo.svg')
 export const Footer = () => (
   <Layout.Footer className={ styles['footer'] }>
     <Divider className={ styles['divider'] }>
-      <img className={ styles['logo'] } alt='color kitty' src={ kitty }/>
+      <img className={ styles['logo'] } alt='color kitty' src={ kitty } />
     </Divider>
-    <p>Made with <Icon type='heart' style={ { color: '#FE615C' } } /> by <a href='https://hopsken.com'>Hopsken</a></p>
+    <p>
+      Made with <Icon type='heart' style={ { color: '#FE615C' } } /> by
+      <a href='https://github.com/hopsken'> Hopsken</a>
+    </p>
   </Layout.Footer>
 )
--- src/views/components/header/header.tsx
+++ src/views/components/header/header.tsx
@@ -10,6 +10,7 @@ export const Header = () => (
       <span>Find perfect palettes from delicious pictures</span>
 
       <div className={ styles['social'] }>
+      <a target='_blank' rel='noopener' href='https://github.com/Hopsken/colorkitty'><Icon type='github' /></a>
         <a target='_blank' rel='noopener' href='https://www.instagram.com/colorkitty_com/'><Icon type='instagram' /></a>
         <a target='_blank' rel='noopener' href='https://twitter.com/@colorkitty_com/'><Icon type='twitter' /></a>
         <a href='mailto:oscar@mg.colorkitty.com'><Icon type='mail' /></a>
