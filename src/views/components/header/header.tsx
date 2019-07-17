da0bfd12780d527565c6bca9610f8f9e2cc2693b
Hopsken <shaowei5@icloud.com>
2019-07-17 21:52:53 +0800

    feat: move header icons to  footer

--- src/views/components/header/header.tsx
+++ src/views/components/header/header.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { Icon, Layout } from 'antd'
+import { Layout } from 'antd'
 
 const brandImg = require('@/views/assets/colorkitty.png')
 const styles = require('./header.styl')
@@ -11,11 +11,4 @@ export const Header = () => (
       <Link to='/'><img alt='colorkitty' src={brandImg} /></Link>
       <Link to='/explore'>Explore</Link>
     </nav>
-
-    <div className={styles['social']}>
-      <a target='_blank' rel='noopener' href='https://github.com/Hopsken/colorkitty'><Icon type='github' /></a>
-      <a target='_blank' rel='noopener' href='https://www.instagram.com/colorkitty_com/'><Icon type='instagram' /></a>
-      <a target='_blank' rel='noopener' href='https://twitter.com/@colorkitty_com/'><Icon type='twitter' /></a>
-      <a href='mailto:oscar@mg.colorkitty.com'><Icon type='mail' /></a>
-    </div>
   </Layout.Header>)
